1247ec117ae32a51fd0c9a65714e33de0f97a4f2
Jesús Paladinez <jesuspaladinez18@gmail.com>
2024-05-10 08:07:41 -0500

    Creación del proyecto

---
+++ index.js
@@ -0,0 +1,39 @@
+function cargarDatos() {
+  fetch("./controllers/traerProductoController.php")
+    .then((response) => response.json())
+    .then((data) => {
+      const tablaDatos = document.getElementById("tablaDatos");
+      tablaDatos.innerHTML = "";
+      data.forEach((row) => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${row.id}</td>
+            <td>${row.nombre}</td>
+            <td>${row.descripcion}</td>
+            <td>
+                <button 
+                    type="button"
+                    class="btn btn-outline-info"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    data-bs-whatever="@getbootstrap">
+                    <ion-icon name="create-outline"></ion-icon>
+                </button>
+                <button class="btn btn-outline-danger" onClick="eliminarProducto(${row.id})">
+                <ion-icon name="trash-outline"></ion-icon>
+                </button>
+            </td>
+            `;
+        tablaDatos.appendChild(tr);
+      });
+    });
+}
+function eliminarProducto(id) {
+  fetch("./controllers/eliminarProductoController.php?id=" + id)
+    .then((response) => response.text())
+    .then((data) => {
+      alert("Ok");
+    });
+}
+cargarDatos();
+
---
+++ models/ProductoDAO.php
@@ -0,0 +1,53 @@
+<?php
+    require('../conexiones/conexion.php');
+    class ProductosDAO{
+       public $id;
+       public $nombre;
+       public $descripcion;
+
+       function __construct($id=null,$nombre=null,$descripcion=null){
+        $this->id=$id;
+        $this->nombre=$nombre;
+        $this->descripcion=$descripcion;
+       } 
+       function traerProductos(){
+        $conn = new Conexion('localhost', 'mariaperalta', '8kyW-NqhU)429CW0', 'mariaperalta');
+        try {
+            $conexion = $conn->Conectar();
+            $stmt=$conexion->query('SELECT * from productos');
+            $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $rows;
+                // foreach ($conexion->query('SELECT * from productos') as $fila) {
+                //     print_r(json_encode($fila));
+                // }  
+        } catch (PDOException $e) {
+            echo "Error al conectarse ====>" . $e;
+        }
+       }
+       function traerProducto($id){
+        $conn = new Conexion('localhost', 'mariaperalta', '8kyW-NqhU)429CW0', 'mariaperalta');
+        try {
+            $conexion = $conn->Conectar();
+            $stmt=$conexion->query('SELECT * from productos WHERE id={$id}');
+            $rows=$stmt->fetch(PDO::FETCH_ASSOC);
+            return $rows;
+                // foreach ($conexion->query('SELECT * from productos') as $fila) {
+                //     print_r(json_encode($fila));
+                // }  
+        } catch (PDOException $e) {
+            echo "Error al conectarse ====>" . $e;
+        }
+       }
+       function eliminarProducto($id){
+        $conn = new Conexion('localhost', 'mariaperalta', '8kyW-NqhU)429CW0', 'mariaperalta');
+        try {
+            $conexion = $conn->Conectar();
+            $stmt = $conexion->prepare("DELETE FROM productos WHERE id = $id");
+            $stmt->execute();
+            return "Exito";  
+        } catch (PDOException $e) {
+            echo "Error al conectarse ====>" . $e;
+        }
+       }
+    }
+?>
